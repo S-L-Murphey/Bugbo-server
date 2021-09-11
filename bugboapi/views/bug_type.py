d6605cc595191f1bbd988a0c812c7a52b99ce146
S-L-Murphey <smurphey1990@gmail.com>
2021-09-11 14:10:37 -0500

    Added create, update, and delete functions for bugtypes

--- bugboapi/views/bug_type.py
+++ bugboapi/views/bug_type.py
@@ -3,11 +3,29 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status
+from django.core.exceptions import ValidationError
 from bugboapi.models import BugType
 
 
 class BugTypeView(ViewSet):
     """Level up bug types"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized bug_type instance
+        """
+        bug_type = BugType()
+        bug_type.label = request.data["label"]
+
+        try:
+            bug_type.save()
+            serializer = BugTypeSerializer(bug_type, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single bug/ticket type
@@ -36,6 +54,38 @@ class BugTypeView(ViewSet):
         serializer = BugTypeSerializer(
             bug_types, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a bug types
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        bug_type = BugType.objects.get(pk=pk)
+        bug_type.label = request.data["label"]
+        bug_type.save()
+
+        # 204 status code means everything worked but the
+        # server is not sending back any data in the response
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single bug_types
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            bug_type = BugType.objects.get(pk=pk)
+            bug_type.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except BugType.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class BugTypeSerializer(serializers.ModelSerializer):
     """JSON serializer for bug/ticket types
